dbf298c3ece0f5c409d4d49825edd252ce8c46c9
RafaelVivanco <rafaelvivanco2@gmail.com>
2026-03-18 01:49:26 -0500

    feat(forms): implement supabase & api connected

---
+++ src/lib/supabase.js
@@ -0,0 +1,8 @@
+import {createClient} from "@supabase/supabase-js";
+import {SUPABASE_CONFIG} from "./config.js";
+
+
+export const supabase = createClient(
+    SUPABASE_CONFIG.url,
+    SUPABASE_CONFIG.key
+)
